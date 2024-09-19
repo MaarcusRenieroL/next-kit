f1bf6f392a3fb2cf1bab77c32f15d5d37ba8e66b
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-09-19 14:40:45 +0530

    init: initialize commander

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
 #!/usr/bin/env node
 
 import { printIntroMessage } from "./utils/message";
+import { Command } from "commander";
+
+const program = new Command();
+program.name("CLI").description("A CLI to get started with your next Next.js Project").version("1.0.0");
 
 printIntroMessage()
+
+program
+	.arguments("[project-name]")
+	.description("Default: Initialize a new project")
+	.action(async (projectName, options) => {
+		console.log("Commander Initialized")
+	});
+
+program
+	.command("init")
+	.description("Initialize a new project")
+	.argument("[project-name]", "Name of the project")
+	.option("--package-manager <pm>", "Specify the package manager to use (npm, pnpm, yarn, bun)")
+	.action(async (projectName, options) => {
+		console.log("Commander Initialized")
+	});
+
+program.parse(process.argv);
