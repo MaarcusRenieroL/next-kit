dc16deafcc450df27552adadb5a1fd9a69c17e60
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-10-03 17:54:58 +0530

    feat: add script for adding trpc

--- src/packages/api/file-contents.ts
+++ src/packages/api/file-contents.ts
@@ -1,3 +1,5 @@
+// api
+
 export const restAPIContent = `
 export const GET = async () => {
   try {
@@ -34,5 +36,129 @@ export const DELETE = async (
   } catch (error) {
     return generateErrorMessage({ error }, 500);
   }
+};
+`
+
+// trpc
+
+export const trpcContent = `
+import { initTRPC, TRPCError } from "@trpc/server";
+import superjson from "superjson";
+import type { FetchCreateContextFnOptions } from "@trpc/server/adapters/fetch";
+
+import { ZodError } from "zod";
+
+export const createTRPCContext = async (opts: FetchCreateContextFnOptions) => {
+  const req = opts.req;
+  return { req };
+};
+
+const t = initTRPC.context<typeof createTRPCContext>().create({
+  transformer: superjson,
+  errorFormatter({ shape, error }) {
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zodError:
+          error.cause instanceof ZodError ? error.cause.flatten() : null,
+      },
+    };
+  },
+});
+
+export const router = t.router;
+export const publicProcedure = t.procedure;
+
+`
+
+export const indexTrpcContent = `
+import type { inferReactQueryProcedureOptions } from "@trpc/react-query";
+import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
+import { router } from "./trpc";
+import { userRouter } from "./routers/users";
+
+export const appRouter = router({ user: userRouter });
+
+export type AppRouter = typeof appRouter;
+export type ReactQueryOptions = inferReactQueryProcedureOptions<AppRouter>;
+export type RouterInputs = inferRouterInputs<AppRouter>;
+export type RouterOutputs = inferRouterOutputs<AppRouter>;
+`
+
+export const usersRouterContent = `
+import { router, publicProcedure } from "../trpc";
+import { TRPCError } from "@trpc/server";
+
+export const userRouter = router({
+	getUser: publicProcedure.query(async ({}) => {
+		return {
+			data: "Success",
+		};
+	});
+})
+`
+
+export const trpcRouteContent = `
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { appRouter } from "~/server/index";
+
+const handler = (req: Request) =>
+  fetchRequestHandler({
+    endpoint: "/api/trpc",
+    req,
+    router: appRouter,
+    createContext: () => ({ req }),
+  });
+
+export { handler as GET, handler as POST };
+
+`
+
+export const trpcProviderContent = `
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+import React, { type FC, type ReactNode, useState } from "react";
+
+import { client } from "~/lib/trpc/client";
+import SuperJSON from "superjson";
+
+type Props = {
+  children?: ReactNode;
+};
+
+export const TRPCProvider: FC<Props> = ({ children }) => {
+  const [queryClient] = useState(() => new QueryClient({}));
+  const [trpcClient] = useState(() =>
+    client.createClient({
+      links: [
+        httpBatchLink({
+          transformer: SuperJSON,
+          url: "/api/trpc",
+        }),
+      ],
+    }),
+  );
+  return (
+    <client.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </client.Provider>
+  );
+};
+
+`
+
+export const providerContent = `
+import type { FC, ReactNode } from "react";
+import { TRPCProvider } from "./trpc-provider";
+
+export const Providers: FC = () => {
+  return (
+    <TRPCProvider>
+      {children}
+    </TRPCProvider>
+  );
 };
 `
--- src/packages/api/index.ts
+++ src/packages/api/index.ts
@@ -1,7 +1,14 @@
 import { CLIOptions } from "../../types";
 import { chdir } from "process";
 import { mkdirSync, writeFileSync } from "fs";
-import { restAPIContent } from "./file-contents";
+import {
+	indexTrpcContent, providerContent,
+	restAPIContent,
+	trpcContent,
+	trpcProviderContent,
+	trpcRouteContent,
+	usersRouterContent
+} from "./file-contents";
 
 export async function createAPIs(options: CLIOptions) {
 	const { api } = options;
@@ -9,19 +16,65 @@ export async function createAPIs(options: CLIOptions) {
 	switch (api) {
 		case "rest":
 			await createRestAPI();
-      break;
-    default:
+			break;
+		case "trpc":
+			await createTrpcAPI();
+			break;
+		default:
 			console.error(`Unsupported API: ${api}`);
 			process.exit(1);
 	}
 }
 
 async function createRestAPI() {
-  chdir("src/app");
+	chdir("src/app");
 	mkdirSync("api");
 	chdir("api")
 	mkdirSync("route")
 	chdir("route")
 	
 	writeFileSync("route.ts", restAPIContent)
+	
+	chdir("../../../")
+}
+
+async function createTrpcAPI() {
+	chdir("src");
+	
+	mkdirSync("server");
+	
+	chdir("server");
+	
+	writeFileSync("trpc.ts", trpcContent);
+	writeFileSync("index.ts", indexTrpcContent);
+	
+	mkdirSync("routers");
+	chdir("routers");
+	
+	writeFileSync("users.ts", usersRouterContent);
+	
+	chdir("../../");
+	
+	chdir("app")
+	
+	mkdirSync("api")
+	chdir("api")
+	
+	mkdirSync("trpc")
+	chdir("trpc")
+	
+	mkdirSync("[trpc]")
+	chdir("[trpc]")
+	
+	writeFileSync("route.ts", trpcRouteContent);
+	
+	chdir("../../../../")
+	
+	mkdirSync("components");
+	chdir("components");
+	mkdirSync("providers")
+	chdir("providers");
+	
+	writeFileSync("trpc-provider.tsx", trpcProviderContent);
+	writeFileSync("index.tsx", providerContent)
 }
