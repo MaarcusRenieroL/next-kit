ce18ea22cb13a2801e22454c7838d0a3b0c8f68b
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-10-08 17:53:43 +0530

    feat: add clerk provider for clerk

--- src/installers/auth/clerk.ts
+++ src/installers/auth/clerk.ts
@@ -4,7 +4,6 @@ import { addPackageDependency } from "@/utils/add-package-dependency.js";
 import fs from "fs-extra";
 import path from "path";
 
-
 export const clerkInstaller: Installer = ({ targetDir, projectName, scopedAppName }) => {
   const projectDir = targetDir ? path.join(targetDir, projectName) : projectName;
 
@@ -20,13 +19,22 @@ export const clerkInstaller: Installer = ({ targetDir, projectName, scopedAppNam
   });
 
   // Copy Clerk-specific files
-  const extrasDir = path.join(PKG_ROOT, "template/extras/auth");
-  const clerkSrc = path.join(extrasDir, "clerk");
+  const extrasDir = path.join(PKG_ROOT, "template/extras");
+  const clerkSrc = path.join(extrasDir, "auth/clerk");
   const clerkDest = path.join(projectDir, scopedAppName === "src" ? "src" : "");
   fs.copySync(clerkSrc, clerkDest);
+
+  const indexPath = path.join(clerkDest, "components/providers/index.tsx");
+
+  if (!fs.existsSync(indexPath)) {
+    const indexProviderContent = fs.readFileSync(path.join(extrasDir, "components/providers/index.tsx"), "utf-8");
+    const updatedContent = indexProviderContent.replace("{children}", `<ClerkProvider>{children}</ClerkProvider>`);
+    fs.writeFileSync(indexPath, 'import { ClerkProvider } from "./clerk-provider"\n' + updatedContent, "utf-8");
+  }
 
   const envContents = `NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=YOUR_PUBLISHABLE_KEY
 CLERK_SECRET_KEY=YOUR_SECRET_KEY`;
 
-  fs.writeFileSync(`${projectDir}/.env`, envContents)
+  fs.writeFileSync(`${projectDir}/.env`, envContents);
+
 }
