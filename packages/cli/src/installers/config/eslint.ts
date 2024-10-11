53590466bb84c6359b6815786a37eeba06cfdfd4
Anonymous Sherlock <layalakash4@gmail.com>
2024-10-11 22:32:30 +0530

    fix: merge issue of remote with local branch

--- packages/cli/src/installers/config/eslint.ts
+++ packages/cli/src/installers/config/eslint.ts
@@ -1,14 +1,9 @@
 import { Installer } from "@/types/global.js";
 import { addPackageDependency } from "@/utils/add-package-dependency.js";
 import path from "path";
-<<<<<<< HEAD
 import { _initialConfig } from "@/../template/extras/config/_eslint.js";
 import fs from "fs-extra";
 export const eslintInstaller: Installer = ({ targetDir, projectName, orm, database: databaseProvider }) => {
-=======
-
-export const eslintInstaller: Installer = ({ targetDir, projectName }) => {
->>>>>>> b4ded0ecfa80173630df4c196a866d4e0d413a87
   const projectDir = targetDir ? path.join(targetDir, projectName) : projectName;
   if (!projectDir) {
     throw new Error("Project directory is required");
@@ -18,7 +13,6 @@ export const eslintInstaller: Installer = ({ targetDir, projectName }) => {
     dependencies: ["eslint", "eslint-config-next", "eslint-config-prettier", "eslint-plugin-check-file", "eslint-plugin-n"],
     devMode: true,
   });
-<<<<<<< HEAD
   if (orm === "drizzle") {
     addPackageDependency({
       projectDir,
@@ -30,11 +24,4 @@ export const eslintInstaller: Installer = ({ targetDir, projectName }) => {
 
   const eslintConfigDest = path.join(projectDir, ".eslintrc.json");
   fs.writeFileSync(eslintConfigDest, JSON.stringify(eslintConfig, null, 2), "utf-8");
-=======
-  addPackageDependency({
-    projectDir,
-    dependencies: ["@prisma/client"],
-    devMode: false,
-  });
->>>>>>> b4ded0ecfa80173630df4c196a866d4e0d413a87
 };
